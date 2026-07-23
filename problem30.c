2555601ce69041b839527cb3cc1ddac941e57306
pratheep1423-max <ppratheep390@gmail.com>
2026-07-23 13:55:44 +0530

    Improve word matching algorithm in problem30.c
    Refactor word counting and matching logic for clarity and performance.

--- problem30.c
+++ problem30.c
@@ -6,46 +6,108 @@ int* findSubstring(char* s, char** words, int wordsSize,
     
     *returnSize = 0;
 
-    int wordLen = strlen(words[0]);
     int sLen = strlen(s);
+    int wordLen = strlen(words[0]);
     int totalLen = wordLen * wordsSize;
 
     int* result = malloc(sLen * sizeof(int));
 
-    for (int i = 0; i <= sLen - totalLen; i++) {
+    // Store the count of each word
+    int* wordCount = calloc(wordsSize, sizeof(int));
+
+    for (int i = 0; i < wordsSize; i++) {
+        for (int j = 0; j < i; j++) {
+            if (strcmp(words[i], words[j]) == 0) {
+                wordCount[j]++;
+                wordCount[i] = -1;
+                break;
+            }
+        }
+
+        if (wordCount[i] == 0)
+            wordCount[i] = 1;
+    }
+
+    // Try each possible offset
+    for (int offset = 0; offset < wordLen; offset++) {
+
+        int left = offset;
+        int right = offset;
+        int count = 0;
 
         int* used = calloc(wordsSize, sizeof(int));
-        int count = 0;
-        int j = i;
 
-        while (count < wordsSize) {
+        while (right + wordLen <= sLen) {
 
-            int found = -1;
+            char current[wordLen + 1];
+            strncpy(current, s + right, wordLen);
+            current[wordLen] = '\0';
 
-            // Check which word matches
-            for (int k = 0; k < wordsSize; k++) {
-                if (!used[k] &&
-                    strncmp(s + j, words[k], wordLen) == 0) {
-                    found = k;
+            int index = -1;
+
+            // Find the word index
+            for (int i = 0; i < wordsSize; i++) {
+                if (strcmp(current, words[i]) == 0) {
+                    index = i;
                     break;
                 }
             }
 
-            if (found == -1)
-                break;
+            if (index == -1 || wordCount[index] == -1) {
+                // Reset window
+                memset(used, 0, wordsSize * sizeof(int));
+                count = 0;
+                left = right + wordLen;
+            }
+            else {
+                used[index]++;
+                count++;
 
-            used[found] = 1;
-            count++;
-            j += wordLen;
-        }
+                // Too many occurrences of this word
+                while (used[index] > wordCount[index]) {
+                    char leftWord[wordLen + 1];
 
-        // All words matched
-        if (count == wordsSize) {
-            result[(*returnSize)++] = i;
+                    strncpy(leftWord, s + left, wordLen);
+                    leftWord[wordLen] = '\0';
+
+                    for (int i = 0; i < wordsSize; i++) {
+                        if (strcmp(leftWord, words[i]) == 0) {
+                            used[i]--;
+                            break;
+                        }
+                    }
+
+                    left += wordLen;
+                    count--;
+                }
+
+                // All words matched
+                if (count == wordsSize) {
+                    result[(*returnSize)++] = left;
+
+                    char leftWord[wordLen + 1];
+
+                    strncpy(leftWord, s + left, wordLen);
+                    leftWord[wordLen] = '\0';
+
+                    for (int i = 0; i < wordsSize; i++) {
+                        if (strcmp(leftWord, words[i]) == 0) {
+                            used[i]--;
+                            break;
+                        }
+                    }
+
+                    left += wordLen;
+                    count--;
+                }
+            }
+
+            right += wordLen;
         }
 
         free(used);
     }
 
+    free(wordCount);
     return result;
 }
