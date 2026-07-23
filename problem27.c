0ce5f531588bf10739b24046128cd38a1256edd4
pratheep1423-max <ppratheep390@gmail.com>
2026-07-23 13:46:56 +0530

    Implement strStr function to find substring

---
+++ problem27.c
@@ -0,0 +1,17 @@
+nt strStr(char* haystack, char* needle) {
+    int i, j;
+
+    for (i = 0; haystack[i] != '\0'; i++) {
+        for (j = 0; needle[j] != '\0'; j++) {
+            if (haystack[i + j] != needle[j]) {
+                break;
+            }
+        }
+
+        if (needle[j] == '\0') {
+            return i;
+        }
+    }
+
+    return -1;
+}
