6ff8ebfaa97fb0e0059322308ebbd3e2ad2c11bb
pratheep1423-max <ppratheep390@gmail.com>
2026-07-27 17:36:46 +0530

    Implement Sudoku solver using backtracking

---
+++ problem37.c
@@ -0,0 +1,38 @@
+#include <stdbool.h>
+
+bool isValid(char** board, int row, int col, char num) {
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num)
+            return false;
+        if (board[i][col] == num)
+            return false;
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num)
+            return false;
+    }
+    return true;
+}
+
+bool solve(char** board) {
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == '.') {
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+
+                        if (solve(board))
+                            return true;
+
+                        board[row][col] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void solveSudoku(char** board, int boardSize, int* boardColSize) {
+    solve(board);
+}
