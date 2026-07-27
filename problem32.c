0e2423e7bea2aae73a7893bfcc67b2de69220471
pratheep1423-max <ppratheep390@gmail.com>
2026-07-27 17:09:12 +0530

    Implement function to find longest valid parentheses

---
+++ problem32.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+int longestValidParentheses(char* s) {
+    int n = 0;
+    while (s[n] != '\0') n++;
+
+    int *stack = (int *)malloc((n + 1) * sizeof(int));
+    int top = 0;
+    stack[top] = -1;
+
+    int maxLen = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(') {
+            stack[++top] = i;
+        } else {
+            top--;
+            if (top < 0) {
+                stack[++top] = i;
+            } else {
+                int len = i - stack[top];
+                if (len > maxLen)
+                    maxLen = len;
+            }
+        }
+    }
+
+    free(stack);
+    return maxLen;
+}
