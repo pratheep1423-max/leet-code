6947abceca4a032f3497719338cf2c3318d7abdd
pratheep1423-max <ppratheep390@gmail.com>
2026-07-28 21:11:26 +0530

    Implement jump function to calculate minimum jumps

---
+++ problem45.c
@@ -0,0 +1,17 @@
+int jump(int* nums, int numsSize) {
+    int jumps = 0;
+    int currentEnd = 0;
+    int farthest = 0;
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (i + nums[i] > farthest)
+            farthest = i + nums[i];
+
+        if (i == currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+}
